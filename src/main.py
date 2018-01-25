9cfd88dc4566c039146f165855c83c7da4f6af88
lin <wl1599@nyu.edu>
2018-01-25 15:52:12 -0500

    weighted loss, Best auc:0.663, acc:0.886, sens:0.904, spec: 0.421

--- src/main.py
+++ src/main.py
@@ -5,20 +5,24 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.optim as optim
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, precision_recall_fscore_support
+from torch.nn.utils import clip_grad_norm
 
 class DeepNet(nn.Module):
 	def __init__(self, input_dim, hidden_dim, num_class = 2):
 		super(DeepNet, self).__init__()
 		self.linear1 = nn.Linear(input_dim, hidden_dim)
-		self.linear2 = nn.Linear(hidden_dim, num_class)
+		self.linear2 = nn.Linear(hidden_dim, int(hidden_dim / 2))
+		self.linear3 = nn.Linear(int(hidden_dim / 2), num_class)
 		self.log_prob = nn.LogSoftmax()
 	
 	def forward(self, x):
 		hid = self.linear1(x)
 		hid = F.relu(hid)
-		out = self.linear2(hid)
-		log_prob = self.log_prob(out)
+		hid = self.linear2(hid)
+		hid = F.relu(hid)
+		hid = self.linear3(hid)
+		log_prob = self.log_prob(hid)
 		return log_prob
 
 def load_data(path):
@@ -70,56 +74,74 @@ def get_stats(pred_y, batch_y):
 	correct = torch.sum(pred_y == batch_y.data)
 	acc = float(correct / total)
 	auc = roc_auc_score(detach_cuda(batch_y).data.numpy(), detach_cuda(pred_y).numpy())
-	return acc, auc
+	sens, spec = precision_recall_fscore_support(detach_cuda(batch_y).data.numpy(), detach_cuda(pred_y).numpy())[1]
+	return acc, auc, sens, spec
+
+def train(model, loss_func, train_batches, test_batches, opt, num_epochs):
+	epoch = 0
+	step = 0
+	best_auc = 0
+	best_spec = 0
+	best_sens = 0
+	best_acc = 0
+	while epoch < num_epochs:
+		batch_X, batch_y = next(train_batches)
+		opt.zero_grad()
+		log_prob = model(batch_X)
+		loss = loss_func(log_prob, batch_y)
+		loss.backward()
+		#clip_grad_norm(model.parameters(), 1)
+		opt.step()
+		step += 1
+		if step >= num_batch:
+			epoch += 1
+			step = 0
+		if step % rpt_step == 0:
+			_, pred_y = log_prob.data.max(dim = 1)
+			acc, auc, sens, spec = get_stats(pred_y, batch_y)
+			#print('Train: step: %d, avg loss: %.3f, acc: %.3f, auc: %.3f,  sens: %.3f, spec: %.3f' % ((step + epoch * num_batch), loss.data[0], acc, auc, sens, spec))
+		if step % test_step == 0:
+			test_batch_X, test_batch_y = next(test_batches)
+			log_prob = model(test_batch_X)
+			_, test_pred_y = log_prob.data.max(dim = 1)
+			acc, auc, sens, spec = get_stats(test_pred_y, test_batch_y)
+			#print('Test: step: %d, acc: %.3f, auc: %.3f, sens: %.3f, spec: %.3f' % ((step + epoch * num_batch), acc, auc, sens, spec))
+			if auc > best_auc:
+				best_auc = auc
+				best_spec = spec
+				best_sens = sens
+				best_acc = acc
+				torch.save(model.state_dict(), 'model.pt')
+	return best_auc, best_acc, best_sens, best_spec
 
 # Load data
-X_train, y_train, X_test, y_test, num_class, input_dim = load_data('data.npz')
+X_train, y_train, X_test, y_test, num_class, input_dim = load_data('b_data.npz')
 
 num_samples = X_train.shape[0]
-batch_size = 100
-num_batch = num_samples / batch_size
+batch_size = 1000
+num_batch = int(num_samples / batch_size)
 hidden_dim = 30
-num_epochs = 100
-learning_rate = 0.05
-rpt_step = 1000
-test_step = 5000
+num_epochs = 50
+learning_rate = 1e-2
+weight_decay = 5e-5
+rpt_step = 10000
+test_step = 50000
+ratio = sum(y_train) / y_train.shape[0] / 10
+weight = set_cuda(torch.FloatTensor([1 / (1 - ratio), 1 / ratio]))
 
 train_batches = batch_iter(X_train, y_train, batch_size, shuffle = True)
 test_batches = batch_iter(X_test, y_test, X_test.shape[0], shuffle = False)
 
 model = DeepNet(input_dim, hidden_dim)
 model = set_cuda(model)
-model.train()
-opt = optim.Adagrad(model.parameters(), lr = learning_rate)
-criterion = nn.CrossEntropyLoss()
+opt = optim.Adagrad(model.parameters(), lr = learning_rate, weight_decay = weight_decay)
+criterion = nn.CrossEntropyLoss(weight = weight)
 
-epoch = 1
-step = 1
-while epoch < num_epochs:
-	batch_X, batch_y = next(train_batches)
-	opt.zero_grad()
-	log_prob = model(batch_X)
-	loss = criterion(log_prob, batch_y)
-	loss.backward()
-	opt.step()
-	step += 1
-	if step >= num_batch:
-		epoch += 1
-		step = 1
-	if step % rpt_step == 0:
-		_, pred_y = log_prob.data.max(dim = 1)
-		acc, auc = get_stats(pred_y, batch_y)
-		print('Train: step: %d, avg loss: %.3f, acc: %.3f, auc: %.3f' % (step, loss.data[0], acc, auc))
-	if step % test_step == 0:
-		batch_X, batch_y = next(test_batches)
-		model.eval()
-		log_prob = model(batch_X)
-		_, pred_y = log_prob.data.max(dim = 1)
-		acc, auc = get_stats(pred_y, batch_y)
-		print('Test: step: %d, acc: %.3f, auc: %.3f' % (step, acc, auc))
-		model.train()
+best_auc, best_acc, best_sens, best_spec = train(model, criterion, train_batches, test_batches, opt, num_epochs)
 
+print('Best auc:%.3f, acc:%.3f, sens:%.3f, spec: %.3f' % (best_auc, best_acc, best_sens, best_spec))
 
+#model.load_state_dict(torch.load('model.pt'))
 
 
 
@@ -136,4 +158,13 @@ target_names = ['Non-Defaulted Loan','Defaulted Loan']
 print(classification_report(y_test,y_pred,target_names=target_names,digits=4))
 print('AUC: ',roc_auc_score(y_test,y_pred))
 '''
-
+'''
+from sklearn.ensemble import RandomForestClassifier
+rf_Gini = RandomForestClassifier(max_depth=10)
+rf_Gini.fit(X_train,y_train)
+y_pred = rf_Gini.predict(X_test)
+print('accuracy: ',accuracy_score(y_test,y_pred))
+target_names = ['Non-Defaulted Loan','Defaulted Loan']
+print(classification_report(y_test,y_pred,target_names=target_names,digits=4))
+print('AUC: ',roc_auc_score(y_test,y_pred))
+'''
