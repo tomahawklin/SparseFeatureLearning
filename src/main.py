afdfd580754c9df9bf839d3b553f3f70a58d99bf
lin <wl1599@nyu.edu>
2018-01-26 16:44:01 -0500

    ep pred acc 53.69 recall 30 74.88 69

--- src/main.py
+++ src/main.py
@@ -73,9 +73,16 @@ def get_stats(pred_y, batch_y):
 	total = batch_y.size()[0]
 	correct = torch.sum(pred_y == batch_y.data)
 	acc = float(correct / total)
-	auc = roc_auc_score(detach_cuda(batch_y).data.numpy(), detach_cuda(pred_y).numpy())
-	sens, spec = precision_recall_fscore_support(detach_cuda(batch_y).data.numpy(), detach_cuda(pred_y).numpy())[1]
-	return acc, auc, sens, spec
+	batch_y = detach_cuda(batch_y).data.numpy()
+	pred_y = detach_cuda(pred_y).numpy()
+	try:
+		auc = roc_auc_score(batch_y, pred_y)
+		sens, spec = precision_recall_fscore_support(batch_y, pred_y)[1]
+		return acc, auc, sens, spec
+	except:
+		auc = float('nan')
+		recall = precision_recall_fscore_support(batch_y, pred_y)[1]
+	return acc, auc, recall
 
 def train(model, loss_func, train_batches, test_batches, opt, num_epochs):
 	epoch = 0
@@ -98,48 +105,61 @@ def train(model, loss_func, train_batches, test_batches, opt, num_epochs):
 			step = 0
 		if step % rpt_step == 0:
 			_, pred_y = log_prob.data.max(dim = 1)
-			acc, auc, sens, spec = get_stats(pred_y, batch_y)
-			#print('Train: step: %d, avg loss: %.3f, acc: %.3f, auc: %.3f,  sens: %.3f, spec: %.3f' % ((step + epoch * num_batch), loss.data[0], acc, auc, sens, spec))
+			if num_class == 2:
+				acc, auc, sens, spec = get_stats(pred_y, batch_y)
+				#print('Train: step: %d, avg loss: %.3f, acc: %.3f, auc: %.3f,  sens: %.3f, spec: %.3f' % ((step + epoch * num_batch), loss.data[0], acc, auc, sens, spec))
+			else:
+				acc, auc, recall = get_stats(pred_y, batch_y)
+				print('Train: step: %d, avg loss: %.3f, acc: %.3f, auc: %.3f' % ((step + epoch * num_batch), loss.data[0], acc, auc))
+				print('Recall:')
+				print(recall)
 		if step % test_step == 0:
 			test_batch_X, test_batch_y = next(test_batches)
 			log_prob = model(test_batch_X)
 			_, test_pred_y = log_prob.data.max(dim = 1)
-			acc, auc, sens, spec = get_stats(test_pred_y, test_batch_y)
-			#print('Test: step: %d, acc: %.3f, auc: %.3f, sens: %.3f, spec: %.3f' % ((step + epoch * num_batch), acc, auc, sens, spec))
-			if auc > best_auc:
+			if num_class == 2:
+				acc, auc, sens, spec = get_stats(test_pred_y, test_batch_y)
+				#print('Test: step: %d, acc: %.3f, auc: %.3f, sens: %.3f, spec: %.3f' % ((step + epoch * num_batch), acc, auc, sens, spec))
+			else:
+				acc, auc, recall = get_stats(test_pred_y, test_batch_y)
+				print('Test: step: %d, acc: %.3f, auc: %.3f' % ((step + epoch * num_batch), acc, auc))
+				print('Recall:')
+				print(recall)
+			if acc > best_acc:
 				best_auc = auc
-				best_spec = spec
-				best_sens = sens
+				#best_spec = spec
+				#best_sens = sens
 				best_acc = acc
-				torch.save(model.state_dict(), 'model.pt')
-	return best_auc, best_acc, best_sens, best_spec
+				torch.save(model.state_dict(), 'e_model.pt')
+	return best_auc, best_acc#, best_sens, best_spec
 
 # Load data
-X_train, y_train, X_test, y_test, num_class, input_dim = load_data('b_data.npz')
+X_train, y_train, X_test, y_test, num_class, input_dim = load_data('e_data.npz')
 
 num_samples = X_train.shape[0]
 batch_size = 1000
 num_batch = int(num_samples / batch_size)
 hidden_dim = 30
-num_epochs = 50
+num_epochs = 100
 learning_rate = 1e-2
 weight_decay = 5e-5
 rpt_step = 10000
 test_step = 50000
 ratio = sum(y_train) / y_train.shape[0] / 10
 weight = set_cuda(torch.FloatTensor([1 / (1 - ratio), 1 / ratio]))
+num_class = 3
 
 train_batches = batch_iter(X_train, y_train, batch_size, shuffle = True)
 test_batches = batch_iter(X_test, y_test, X_test.shape[0], shuffle = False)
 
-model = DeepNet(input_dim, hidden_dim)
+model = DeepNet(input_dim, hidden_dim, num_class)
 model = set_cuda(model)
 opt = optim.Adagrad(model.parameters(), lr = learning_rate, weight_decay = weight_decay)
-criterion = nn.CrossEntropyLoss(weight = weight)
+criterion = nn.CrossEntropyLoss()#weight = weight)
 
-best_auc, best_acc, best_sens, best_spec = train(model, criterion, train_batches, test_batches, opt, num_epochs)
+best_auc, best_acc = train(model, criterion, train_batches, test_batches, opt, num_epochs)
 
-print('Best auc:%.3f, acc:%.3f, sens:%.3f, spec: %.3f' % (best_auc, best_acc, best_sens, best_spec))
+print('Best auc:%.3f, acc:%.3f' % (best_auc, best_acc))
 
 #model.load_state_dict(torch.load('model.pt'))
 
