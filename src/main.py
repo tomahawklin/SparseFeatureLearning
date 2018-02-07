2008420ec6178db33ca3817d8b913298f7e15202
lin <wl1599@nyu.edu>
2018-02-06 22:34:06 -0500

    Notes

--- src/main.py
+++ src/main.py
@@ -102,7 +102,11 @@ def train(model, loss_func, train_batches, test_batches, opt, num_epochs):
 # Load data
 train_data, test_data, embed_dict, embed_dims, embed_keys, float_keys = load_data('data_final.npz')
 embed_sizes = {k: 4 for k in embed_keys}
-
+'''
+embed_sizes = {'issue_month': 4, 'home_ownership': 2, 'verification_status': 2, 'emp_length': 4, 
+               'initial_list_status': 1, 'addr_state': 10, 'early_month': 4, 'grade': 3, 
+               'purpose': 5, 'sub_grade': 10, 'zip_code': 20, 'early_year': 6, 'term': 1, 'issue_year': 6}
+'''
 num_samples = train_data.shape[0]
 batch_size = 1000
 num_batch = int(num_samples / batch_size)
@@ -118,19 +122,42 @@ weight = set_cuda(torch.FloatTensor([1 / ratio_0, 1 / ratio_1]))
 num_class = len(set([t['label'] for t in train_data]))
 
 train_batches = batch_iter(train_data, batch_size, embed_keys, float_keys, shuffle = False)
-test_batches = batch_iter(test_data, 1000, embed_keys, float_keys, shuffle = False)
+test_batches = batch_iter(test_data, len(test_data), embed_keys, float_keys, shuffle = False)
 
 model = DeepNet(len(float_keys), embed_dims, embed_sizes, hidden_dim, embed_keys, num_class)
 model = set_cuda(model)
 opt = optim.Adagrad(model.parameters(), lr = learning_rate, weight_decay = weight_decay)
 # TODO: experiment with different weights and dorpout
-criterion = nn.CrossEntropyLoss()#weight = weight)
+criterion = nn.CrossEntropyLoss(weight = weight)
 
 best_auc, best_acc, best_sens, best_spec = train(model, criterion, train_batches, test_batches, opt, num_epochs)
 
 print('Best auc:%.3f, acc:%.3f' % (best_auc, best_acc))
 
-#model.load_state_dict(torch.load('model.pt'))
+model.load_state_dict(torch.load('deep_model.pt'))
+test_X_float, test_X_embed, test_y = next(test_batches)
+log_prob = model(test_X_float, test_X_embed)
+_, test_pred_y = log_prob.data.max(dim = 1)
+score = log_prob[:, 1]
+acc, auc, sens, spec = get_stats(test_pred_y, test_y, score)
 
 
 
+
+
+'''
+With 1 / ratio weights, 100 epochs, 10000 batch_size, 1e-2 lr:
+Best auc:0.835, acc:0.831
+With 1 / ratio weights, 50 epochs, 10000 batch_size, 1e-2 lr:
+Best auc:0.742, acc:0.796
+Equal weights, 100 epochs, 10000 batch_size, 1e-2 lr:
+Best auc:0.889, acc:0.862
+Equal weights, 100 epochs, 1000 batch_size, 1e-2 lr:
+Best auc:0.937, acc:0.896
+with 1 / ratio weights, 1000 batch_size, 1e-2 lr:
+Best auc:0.933, acc:0.892
+with 1 / ratio weights, 500 batch_size, 1e-2 lr:
+Best auc: 0.940, acc:0.890
+with 1 / ratio weights, 1000 batch_size, 5e-3 lr:
+Best auc: 0.939, acc:0.882
+'''
