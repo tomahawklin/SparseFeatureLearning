49948c9473c4972ab19cc9139d4fd5ff49dfedd0
lin <wl1599@nyu.edu>
2018-02-25 15:12:53 -0500

    Bug fix

--- src/ratio.py
+++ src/ratio.py
@@ -12,7 +12,7 @@ from sklearn.ensemble import RandomForestClassifier
 import time
 
 class MultiNet(nn.Module):
-    def __init__(self, input_float_dim, embed_dims, embed_sizes, hidden_dim, embed_keys, num_class, lmbda = 0.1):
+    def __init__(self, input_float_dim, embed_dims, embed_sizes, hidden_dim, embed_keys, lmbda = 0.1):
         super(MultiNet, self).__init__()
         self.linear1 = nn.Linear(input_float_dim, hidden_dim)
         # Embed_keys makes sure that order of layers are consistent
@@ -22,7 +22,7 @@ class MultiNet(nn.Module):
         sub_hidden_dim = int(hidden_dim / 2)
         self.linear3 = nn.Linear(hidden_dim, sub_hidden_dim)
         self.linear4 = nn.Linear(sub_hidden_dim, 1)
-        self.rgrs_loss = nn.L1Loss()
+        self.rgrs_loss = nn.MSELoss()
         
     def forward(self, X_float, X_embed, batch_label, batch_ratio):
         float_hid = self.linear1(X_float)
@@ -43,11 +43,11 @@ def get_stats(ratio_pred, ratio_target):
     diff = np.abs(ratio_target - ratio_pred)
     return np.mean(diff), np.median(diff), np.max(diff)
 
-def train(model, train_batches, test_batches, opt, num_epochs, verbose = True):
+def train(model, train_batches, test_batches, opt, num_epochs, clf, verbose = True):
     epoch = 0
     step = 0
     best_epoch = 0
-    best_diff_median = 100
+    best_auc = 0
     rpt_step = 5 * num_batch
     test_step = 10 * num_batch
     while epoch < num_epochs:
@@ -70,15 +70,19 @@ def train(model, train_batches, test_batches, opt, num_epochs, verbose = True):
             loss, ratio = model(test_X_float, test_X_embed, test_label, test_ratio)
             if verbose:
                 mean, median, mmax = get_stats(ratio, test_ratio)
-                print('Test: step: %d, loss: %.3f, diff mean: %.3f, median: %.3f, max: %.3f' % ((step + epoch * num_batch), loss.data[0], mean, median, mmax))
-            if median < best_diff_median:
+                X_test = ratio.data.cpu().numpy().reshape(-1, 1)
+                y_test = test_label.data.cpu().numpy().reshape(-1)
+                pos_prob = clf.predict_proba(X_test)[:, 1]
+                auc = roc_auc_score(y_test, pos_prob)
+                print('Test: step: %d, auc: %.3f, loss: %.3f, diff mean: %.3f, median: %.3f, max: %.3f' % ((step + epoch * num_batch), auc, loss.data[0], mean, median, mmax))
+            if auc > best_auc:
+                best_auc = auc
                 best_diff_mean = mean
                 best_diff_median = median
                 best_diff_max = mmax
                 best_ratio = ratio
                 best_epoch = epoch
-                torch.save(model.state_dict(), 'ratio_model.pt')
-    return best_diff_mean, best_diff_median, best_diff_max, best_ratio, best_epoch
+    return best_auc, best_diff_mean, best_diff_median, best_diff_max, best_ratio, best_epoch
 
 # Load data
 train_data, test_data, embed_dict, embed_dims, embed_keys, float_keys = load_data('final_data.npz')
@@ -91,32 +95,35 @@ num_batch = int(num_samples / batch_size)
 hidden_dim = 30
 num_epochs = 300
 learning_rate = 1e-2
-weight_decay = 5e-5
-ratio_1 = sum([1 for t in train_data if t['label'] == 1]) / train_data.shape[0]
-ratio_0 = 1 - ratio_1
-weight = set_cuda(torch.FloatTensor([1 / ratio_0, 1 / ratio_1]))
-num_class = len(set([t['label'] for t in train_data]))
 
+X_train = np.array([d['pymnt_ratio'] for d in train_data]).reshape(-1, 1)
+y_train = np.array([d['label'] for d in train_data])
+X_test = np.array([d['pymnt_ratio'] for d in test_data]).reshape(-1, 1)
+y_test = np.array([d['label'] for d in test_data])
+rf_en = RandomForestClassifier(max_depth = 10, criterion = 'entropy')
+rf_en.fit(X_train, y_train)
+
+global_auc = 0
 for hidden_dim in [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
     train_batches = batch_iter_mul(train_data, batch_size, embed_keys, float_keys, shuffle = False)
     test_batches = batch_iter_mul(test_data, len(test_data), embed_keys, float_keys, shuffle = False)
     
-    model = MultiNet(len(float_keys), embed_dims, embed_sizes, hidden_dim, embed_keys, num_class)
+    model = MultiNet(len(float_keys), embed_dims, embed_sizes, hidden_dim, embed_keys)
     model = set_cuda(model)
-    opt = optim.Adagrad(model.parameters(), lr = learning_rate, weight_decay = weight_decay)
+    opt = optim.Adagrad(model.parameters(), lr = learning_rate)
     
     start = time.time()
-    best_diff_mean, best_diff_median, best_diff_max, best_ratio, best_epoch = train(model, train_batches, test_batches, opt, num_epochs)
-    
-    print('Best diff mean: %.3f, median: %.3f, max: %.3f in epoch %d. Finished in %.3f seconds' % (best_diff_mean, best_diff_median, best_diff_max, best_epoch, time.time() - start))
+    best_auc, best_diff_mean, best_diff_median, best_diff_max, best_ratio, best_epoch = train(model, train_batches, test_batches, opt, num_epochs, rf_en)
+    print('Best AUC: %.3f, diff mean: %.3f, median: %.3f, max: %.3f in epoch %d. Finished in %.3f seconds' % (best_auc, best_diff_mean, best_diff_median, best_diff_max, best_epoch, time.time() - start))
+    if best_auc > global_auc:
+        #torch.save(model.state_dict(), 'ratio_model.pt')
+        global_auc = best_auc
 
-
+'''
 model.load_state_dict(torch.load('ratio_model.pt'))
 test_X_float, test_X_embed, test_label, test_duration, test_ratio = next(test_batches)
 loss, ratio = model(test_X_float, test_X_embed, test_label, test_ratio)
 mean, median, mmax = get_stats(ratio, test_ratio)
-
-
 
 batch_size = 20541
 num_batch = int(len(train_data) / batch_size)
@@ -140,7 +147,7 @@ print('AUC: ', roc_auc_score(y_test, pos_prob))
 #AUC:  0.616531154066
 #TODO: It is weird that X_test gives lower abs diff but poorer AUC
 #0.190357756058 0.0568775107673 1.31790662755
-
+'''
 '''
 First benchmark: classification with raw features
 keys = embed_keys + float_keys
@@ -230,11 +237,7 @@ AUC:  0.83112982664
 
 
 Notes of best median abs error (median and mean abs error decrease but max abs error increase over time)
-L1Loss:
-Dim 10:
-Best diff: 0.058 in epoch 250.
-Dim 20:
-Best diff: 0.059 in epoch 230.
-Dim 30:
-Best diff: 0.057 in epoch 230.
+MSELoss:
+Dim10:
+Best AUC: 0.803, diff mean: 0.187, median: 0.137, max: 2.566 in epoch 270
 '''
